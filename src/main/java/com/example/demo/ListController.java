625b8d89f6a5b8ecc9517e5f7b7f2cffd6859245
Ross Llewallyn <rossllewallyn@proton.me>
2025-07-01 10:48:36 -0400

    fixed 1 warning

--- src/main/java/com/example/demo/ListController.java
+++ src/main/java/com/example/demo/ListController.java
@@ -8,7 +8,6 @@ import java.util.Optional;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.stream.Collectors;
 
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
